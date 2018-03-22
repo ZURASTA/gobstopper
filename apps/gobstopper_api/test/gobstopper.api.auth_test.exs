23d06e6a87ebb4543fbd2675e34b88bb74516661
Stefan Johnson <sj@scrimpycat.io>
2018-03-22 17:02:37 +1100

    Test all_credentials/1 API

--- apps/gobstopper_api/test/gobstopper.api.auth_test.exs
+++ apps/gobstopper_api/test/gobstopper.api.auth_test.exs
@@ -26,4 +26,30 @@ defmodule Gobstopper.API.AuthTest do
         assert nil == Auth.verify(token)
         assert identity == Auth.verify(token2)
     end
+
+    describe "all_credentials/1" do
+        test "retrieving all credentials associated with a non-existent identity" do
+            assert { :error, "Invalid token" } == Auth.all_credentials(nil)
+        end
+
+        test "retrieving all credentials associated with an identity with no credentials" do
+            identity = Gobstopper.Service.Repo.insert!(Gobstopper.Service.Auth.Identity.Model.changeset(%Gobstopper.Service.Auth.Identity.Model{}))
+            { :ok, token, _ } = Guardian.encode_and_sign(identity)
+
+            assert { :ok, credentials } = Auth.all_credentials(token)
+            assert Enum.all?(credentials, fn
+                { _, { :none, nil } } -> true
+                _ -> false
+            end)
+        end
+
+        test "retrieving all credentials associated with an identity with an email credential" do
+            { :ok, token } = Auth.Email.register("foo@bar", "secret")
+            assert { :ok, credentials } = Auth.all_credentials(token)
+            assert Enum.any?(credentials, fn
+                { :email, { :unverified, "foo@bar" } } -> true
+                _ -> false
+            end)
+        end
+    end
 end
